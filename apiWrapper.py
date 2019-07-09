545f9b3daa92f8e5bb9855a9d997343da30e73cc
BYaka <byaka.life@gmail.com>
2019-07-10 01:36:04 +0300

    Just added.

---
+++ apiWrapper.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+from functionsex import *
+
+from errors import *
+
+class ApiWrapperBase(type):
+   """
+   This Meta-Class wraps each public method of Api and do some protocol-specific magic or add authentication support.
+
+   :note:
+      Auth can be disabled for whole class by attr `_apiWrappingDisable_auth`.
+
+   :note:
+      Call-wrapping can be disabled for whole class by attr `_apiWrappingDisable_call`.
+   """
+
+   def __new__(meta, className, bases, classDict):
+      for k, v in classDict.items():
+         if not k.startswith('_') and callable(v):
+            classDict[k]=ApiWrapperBase.wrap(
+               classDict, v,
+               disableAuth=classDict.get('_apiWrappingDisable_auth', False),
+               disableCall=classDict.get('_apiWrappingDisable_call', False),
+            )
+      return type.__new__(meta, className, bases, classDict)
+
+   @classmethod
+   def wrap(cls, classDict, f, disableAuth=False, disableCall=False):
+      @functools.wraps(f)
+      def fNew(self, *args, **kwargs):
+         scope=cls._wrapped_pre({}, f, self, args, kwargs)
+         if not disableAuth:
+            cls._wrapped_auth(scope, f, self, args, kwargs)
+         if not disableCall:
+            return cls._wrapped_call(scope, f, self, args, kwargs)
+         else:
+            return f(self, *args, **kwargs)
+      fNew._original=f
+      fNew=cls._wrapping(classDict, fNew, f)
+      return fNew
+
+   @classmethod
+   def _wrapping(cls, classDict, fNew, fOld):
+      return fNew
+
+   @classmethod
+   def _wrapped_pre(cls, scope, f, self, args, kwargs):
+      return scope
+
+   @classmethod
+   def _wrapped_auth(cls, scope, f, self, args, kwargs):
+      pass
+
+   @classmethod
+   def _wrapped_call(cls, scope, f, self, args, kwargs):
+      return f(self, *args, **kwargs)
+
+
+from jsonrpc import dispatcher
+
+class ApiWrapperJSONRPC(ApiWrapperBase):
+   #~ это имплементация под https://pypi.org/project/json-rpc/
+
+   @classmethod
+   def _wrapping(cls, classDict, fNew, fOld):
+      if not hasattr(classDict, '_JSONRPC_dispatcherMap'):
+         setattr(classDict, '_JSONRPC_dispatcherMap', dispatcher)
+      classDict.dispatcher[fOld.__name__]=fNew
+      return fNew
+
+   @classmethod
+   def _prepBadResp(cls, e):
+      if isinstance(e, AssertionError):
+         return {'code':-6, 'data':ERROR_MSG[-6]}
+      elif isinstance(e, NotImplementedError):
+         return {'code':-1, 'data':ERROR_MSG[-1]}
+      elif isinstance(e, ScreendeskError):
+         return {'code':e.code, 'data':e.msg}
+      else:
+         return {'code':False, 'data':getErrorInfo(fallback=False)}
+
+   @classmethod
+   def _prepResp(cls, data):
+      if isinstance(data, Exception):
+         return cls._prepBadResp(data)
+      else:
+         return {'code':True, 'data':data}
+
+   @classmethod
+   def _wrapped_pre(cls, scope, f, self, args, kwargs):
+      scope['conn']=kwargs.get('_conn', {})  #! это завязано на старом FJSONRPC протоколе
+      return scope
+
+   @classmethod
+   def _wrapped_auth(cls, scope, f, self, args, kwargs):
+      pass
+
+   @classmethod
+   def _wrapped_call(cls, scope, f, self, args, kwargs):
+      if scope['conn'].get('__getRawResponse', False):
+         r=f(self, *args, **kwargs)
+      else:
+         try:
+            r=cls._prepResp(f(self, *args, **kwargs))
+         except Exception, e:
+            r=cls._prepBadResp(e)
+      return r
